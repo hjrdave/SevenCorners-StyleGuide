ec73990feed84815bc4c4674e88882293746df24
David Sanders <dsanders_m633@outlook.com>
2023-03-22 09:40:26 -0400

    added striped hero pattern

--- src/components/atoms/HeroBanner/HeroBanner.tsx
+++ src/components/atoms/HeroBanner/HeroBanner.tsx
@@ -3,17 +3,18 @@ import { Row, Col } from 'react-bootstrap';
 import styles from './HeroBanner.module.scss';
 
 interface Props{
-
+    type: 'typography' | 'stripes'
 }
-export default function HeroBanner({ }: Props) {
+export default function HeroBanner({ type }: Props) {
 
+    const pattern = (type === 'stripes') ? 'sc-bg-stripes' : 'sc-bg-typography';
     return (
         <>
-            <Row className={`d-flex justify-content-center sc-bg-typography ${styles.heroSize}`}>
+            <Row className={`d-flex justify-content-center ${pattern} ${styles.heroSize}`}>
                 <Col sm={12} md={8} className={`d-flex align-items-center p-5 px-md-0`}>
                     <div>
-                        <h1 className={"text-center text-md-start"}>This is a Hero Banner</h1>
-                        <p className={'text-center text-md-start fs-3'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                        <h1 className={"text-center text-md-start text-dark"}>This is a Hero Banner</h1>
+                        <p className={'text-center text-md-start fs-3 text-dark'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                     </div>
                 </Col>
             </Row>
