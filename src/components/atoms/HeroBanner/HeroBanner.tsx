d6822c11aa730795173e48f617bb3b1544aa3125
David Sanders <dsanders_m633@outlook.com>
2023-03-24 10:13:09 -0400

    updated hero banner snippet

--- src/components/atoms/HeroBanner/HeroBanner.tsx
+++ src/components/atoms/HeroBanner/HeroBanner.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 import styles from './HeroBanner.module.scss';
 
 interface Props{
@@ -10,14 +10,18 @@ export default function HeroBanner({ type }: Props) {
     const pattern = (type === 'stripes') ? 'sc-bg-stripes' : 'sc-bg-typography';
     return (
         <>
-            <Row className={`d-flex justify-content-center ${pattern} ${styles.heroSize}`}>
-                <Col sm={12} md={8} className={`d-flex align-items-center p-5 px-md-0`}>
-                    <div>
-                        <h1 className={"text-center text-md-start text-dark"}>This is a Hero Banner</h1>
-                        <p className={'text-center text-md-start fs-3 text-dark'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                    </div>
-                </Col>
-            </Row>
+            <div className={`${styles.heroSize} ${pattern} px-5`}>
+                <Container>
+                    <Row>
+                        <Col sm={12} className={`d-flex align-items-center p-5 px-md-0`}>
+                            <div>
+                                <h1 className={"text-center text-md-start text-dark"}>This is a Hero Banner</h1>
+                                <p className={'text-center text-md-start fs-3 text-dark'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>  
         </>
     )
 }
